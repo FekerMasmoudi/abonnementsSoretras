d06fefae6a77fe67c7bec18d282219a9ab830e21
FekerMasmoudi <feker.masmoudi@gmail.com>
2023-10-12 15:40:43 +0100

    Initial version of centredb generated by generator-jhipster@7.9.3

---
+++ src/main/java/tn/soretras/centremanager/config/Constants.java
@@ -0,0 +1,11 @@
+package tn.soretras.centremanager.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
